cacbfba60259b472896d7727cac68b488d594f74
ASUS <anahita_fakhravar@yahoo.com>
2021-08-02 10:29:19 +0430

    Create file for strigns and define some strings

---
+++ src/assets/Strings.js
@@ -0,0 +1,17 @@
+//All project strings are defined in this file
+
+export const Strings = {
+
+  //Messages
+  wrongEmail: 'Invalid email address!',
+  emptyFullName: 'Please enter your full name.',
+  emptyEmail: 'Please enter your email address.',
+
+  //General
+  confirm: 'Confirm',
+
+  //Authentication
+  fullName: 'Full name',
+  email: 'Email',
+
+};
