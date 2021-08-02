0c2c9ac31b3779b217b8bc981c02ef3d8dd7823c
ASUS <anahita_fakhravar@yahoo.com>
2021-08-02 10:29:00 +0430

    Create file for colors

---
+++ src/assets/Colors.js
@@ -0,0 +1,15 @@
+//All project colors are defined in this file
+
+export const Colors = {
+
+    darkTheme: '#003366',
+    mediumTheme: '#A03737',
+    lightTheme: '#F9F871',
+
+    lightBackGround: '#F6F8F8',
+  
+    txtOnLightTheme: '#290200',
+    txtOnDarkTheme: '#F6F8F8',
+    txtPlaceholder: '#d6d4d4',
+  
+};
