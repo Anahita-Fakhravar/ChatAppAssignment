88b024bf2febf1e6d2537c75c6eaf92aa76f3f2f
ASUS <anahita_fakhravar@yahoo.com>
2021-08-02 10:43:42 +0430

    Create redux store

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+//Create store 
+
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const middleWares = [sagaMiddleware];
+const store = createStore(rootReducer, applyMiddleware(...middleWares));
+sagaMiddleware.run(rootSaga);
+export { store };
